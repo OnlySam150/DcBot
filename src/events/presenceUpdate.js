e26ba61aa7f3c7f2befc4fc4be9edfb1346752d8
Sam <samjaeger006@gmail.com>
2026-01-03 07:13:43 +0100

    bug fix ad system

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -5,29 +5,17 @@ export default {
   name: Events.PresenceUpdate,
   async execute(oldPresence, newPresence) {
     newPresence.activities.forEach(async (activity) => {
-      if (activity.type === ActivityType.Custom) {
-        console.log(
-          `oldPresence: ${
-            oldPresence?.activities.find((a) => a.type === 4)?.state
-          }, newPresence: ${
-            newPresence?.activities.find((a) => a.type === 4)?.state
-          }`
-        );
-        if (
-          oldPresence?.activities.find((a) => a.type === 4)?.state ===
-          newPresence?.activities.find((a) => a.type === 4)?.state
-        ) {
-          return;
-        } else {
-          await update_ad_status(
-            newPresence.user,
-            newPresence?.activities.find((a) => a.type === 4)?.state
-          );
-          console.log("Custom activity found and ad status updated.");
-        }
-      } else {
-        console.log("No custom activity found.");
+      if (
+        oldPresence?.activities.find((a) => a.type === 4)?.state ===
+        newPresence?.activities.find((a) => a.type === 4)?.state
+      ) {
         return;
+      } else {
+        await update_ad_status(
+          newPresence.user,
+          newPresence?.activities.find((a) => a.type === 4)?.state
+        );
+        console.log("Custom activity found and ad status updated.");
       }
     });
   },
