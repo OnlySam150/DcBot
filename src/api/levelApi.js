6fdbd77ceb7dce44d53fcca2fb16008f242c6d14
Sam <samjaeger006@gmail.com>
2026-01-18 12:12:54 +0100

    fix(levelsystem): Changed userid to id

--- src/api/levelApi.js
+++ src/api/levelApi.js
@@ -2,7 +2,7 @@ import { pool } from "../utils/db.js";
 
 export const getLevelData = async (userId) => {
   try {
-    const sql = `SELECT * FROM level WHERE userId = $1`;
+    const sql = `SELECT * FROM level WHERE id = $1`;
     const values = [userId];
     const result = await pool.query(sql, values);
     return result.rows[0];
@@ -13,7 +13,7 @@ export const getLevelData = async (userId) => {
 
 export const updateLevelData = async (userId, newLevel, newXP) => {
   try {
-    const sql = `INSERT INTO level (level, xp, userId) VALUES ($1, $2, $3) ON CONFLICT (userId) DO UPDATE SET level = EXCLUDED.level, xp = EXCLUDED.xp`;
+    const sql = `INSERT INTO level (level, xp, id) VALUES ($1, $2, $3) ON CONFLICT (id) DO UPDATE SET level = EXCLUDED.level, xp = EXCLUDED.xp`;
     const values = [newLevel, newXP, userId];
     await pool.query(sql, values);
   } catch (err) {
