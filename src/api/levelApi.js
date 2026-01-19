01795598242274d92d0eb3cb2739ce1d84da55b9
Sam <samjaeger006@gmail.com>
2026-01-19 06:29:41 +0100

    fix(levelsystem): add limit to SELECT SQL

--- src/api/levelApi.js
+++ src/api/levelApi.js
@@ -1,12 +1,12 @@
 import { pool } from "../utils/db.js";
 
-export const getLevelData = async (userId = null) => {
+export const getLevelData = async (userId = null, limit = 10) => {
   try {
     const leaderboard = userId ? false : true;
     const sql = leaderboard
-      ? `SELECT * FROM level ORDER BY xp DESC`
+      ? `SELECT * FROM level ORDER BY xp DESC LIMIT $1`
       : `SELECT * FROM level WHERE id = $1`;
-    const values = leaderboard ? [] : [userId];
+    const values = leaderboard ? [limit] : [userId];
     const result = await pool.query(sql, values);
     return leaderboard ? result.rows : result.rows[0];
   } catch (err) {
