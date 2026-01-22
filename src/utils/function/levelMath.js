543eca515bbaa47838573401b57b8c4c4b356563
Sam <samjaeger006@gmail.com>
2026-01-22 16:52:13 +0100

    feat(levelSystem): New math formula that calculates the current level based on XP

--- src/utils/function/levelMath.js
+++ src/utils/function/levelMath.js
@@ -4,6 +4,23 @@ export const levelCalculateFunction = (level) => {
   return { levelFormel };
 };
 
+export const yLevelCalulate = (xp) => {
+  let level;
+  const a = 20;
+  const b = -40;
+  const c = 55 - xp;
+
+  const diskriminante = b * b - 4 * a * c;
+
+  if (diskriminante < 0) {
+    return (level = 0);
+  }
+
+  const x = (-b + Math.sqrt(diskriminante)) / (2 * a);
+
+  return (level = Math.floor(x));
+};
+
 export const checkLevelUp = (currentXp, needXp) => {
   let leveledUp;
   if (currentXp >= needXp) {
